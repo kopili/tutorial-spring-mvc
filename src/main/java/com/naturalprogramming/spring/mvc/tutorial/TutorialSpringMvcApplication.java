1abf8dfc055895703952bbeb1c529b5ab0981906
Borislav Kirilov <bobikirilov@gmail.com>
2017-02-10 16:24:47 +0200

    Initial commit of the base project created by Sprint Tool Suite

---
+++ src/main/java/com/naturalprogramming/spring/mvc/tutorial/TutorialSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package com.naturalprogramming.spring.mvc.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialSpringMvcApplication.class, args);
+	}
+}
