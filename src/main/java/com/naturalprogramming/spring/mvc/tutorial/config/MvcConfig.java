fa7dbdc3b451931440e7f757e8e291e6d067bc49
Borislav Kirilov <bobikirilov@gmail.com>
2017-02-10 17:52:55 +0200

    Added default controller 'home' via a configuration class. Also added initial JSP file and specified its location in application.properties

---
+++ src/main/java/com/naturalprogramming/spring/mvc/tutorial/config/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.naturalprogramming.spring.mvc.tutorial.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		//super.addViewControllers(registry);
+		registry.addViewController("/").setViewName("home");
+	}
+}
---
+++ src/main/java/com/naturalprogramming/spring/mvc/tutorial/controllers/RootController.java
@@ -0,0 +1,17 @@
+package com.naturalprogramming.spring.mvc.tutorial.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RootController {
+
+	private static final Logger logger = LoggerFactory.getLogger(RootController.class);
+	
+	/*@RequestMapping("/")
+	public String home() {
+		return "home";
+	}*/
+	
+}
