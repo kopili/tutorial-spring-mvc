53de58638d493bd21e9e67290c9f603589dc6582
Borislav Kirilov <bobikirilov@gmail.com>
2017-02-13 16:46:53 +0200

    Added some info pages in comments about what auto-generated queries are supported by Spring as well as how to make custom queries

--- src/main/java/com/naturalprogramming/spring/mvc/tutorial/repositories/UserRepository.java
+++ src/main/java/com/naturalprogramming/spring/mvc/tutorial/repositories/UserRepository.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.naturalprogramming.spring.mvc.tutorial.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	// More info about supported keywords inside method names:
+	// http://docs.spring.io/spring-data/jpa/docs/1.9.6.RELEASE/reference/html/#jpa.query-methods.query-creation
+	// Also if you don't want the query to be auto-generated, use this approach:
+	// http://docs.spring.io/spring-data/jpa/docs/1.9.6.RELEASE/reference/html/#jpa.query-methods.at-query
 	User findByEmail(String email);
 }
