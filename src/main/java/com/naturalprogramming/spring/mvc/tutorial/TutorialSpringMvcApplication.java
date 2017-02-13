f610125fa9dff90a0441511c91fcb3d71b9eaf88
Borislav Kirilov <bobikirilov@gmail.com>
2017-02-13 17:09:32 +0200

    Adding transactions to the functionality that saves form data

--- src/main/java/com/naturalprogramming/spring/mvc/tutorial/TutorialSpringMvcApplication.java
+++ src/main/java/com/naturalprogramming/spring/mvc/tutorial/TutorialSpringMvcApplication.java
@@ -2,8 +2,10 @@ package com.naturalprogramming.spring.mvc.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class TutorialSpringMvcApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/naturalprogramming/spring/mvc/tutorial/services/UserServiceImpl.java
+++ src/main/java/com/naturalprogramming/spring/mvc/tutorial/services/UserServiceImpl.java
@@ -1,13 +1,17 @@
 package com.naturalprogramming.spring.mvc.tutorial.services;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.naturalprogramming.spring.mvc.tutorial.dto.SignupForm;
 import com.naturalprogramming.spring.mvc.tutorial.entities.User;
 import com.naturalprogramming.spring.mvc.tutorial.repositories.UserRepository;
 
 @Service
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 public class UserServiceImpl implements UserService {
 
 	private UserRepository userRepository;
@@ -18,6 +22,7 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public void signup(SignupForm signupForm) {
 		User user = new User();
 		user.setEmail(signupForm.getEmail());
